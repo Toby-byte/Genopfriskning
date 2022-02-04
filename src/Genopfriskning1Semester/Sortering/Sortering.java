2eedbe6618759adee77f583c16a33d9fd60a2d52
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-02-04 11:25:25 +0100

    Sortering beskrivning

---
+++ src/Genopfriskning1Semester/Sortering/Sortering.java
@@ -0,0 +1,13 @@
+package Genopfriskning1Semester.Sortering;
+
+//SorteringSkriven metode, der indlæser 5 tekststrenge fra tastaturetog
+//udskriver dem på skærmen i alfabetisk faldende orden på skærmen.
+//Hvis der indlæses:
+//abekat musefælde ananas slut solskin
+
+//skal der udskrives følgende på skærmen:
+//solskin slut musefælde ananas abekat
+
+
+public class Sortering {
+}
